a109ce7a938bb87cc376d103a51a5a1970427d82
kassrojas <kassandrarojass@gmail.com>
2022-12-12 20:25:45 -0500

    input city on browser, successfully recieve weatherResults in console log

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,20 @@
 var cityInput = document.querySelector('#cityInput');
 var searchButton = document.querySelector('#searchButton');
 
-// var cityName = 'new york';
+function displayData (results){
+    for (var result of results){
+        console.log(results);
+    };
+};
+
+
+
+
+
+
+
+
+
 // geocodeUrl gives back data on lat and lon
 function getCoordinates (cityName){
     var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=0cf0c8196ec606a9f30889804aba9ea1';
@@ -22,47 +35,49 @@ function getCoordinates (cityName){
 
 // transfer coordinates data into getWeather
 function getWeather (lat, lon){
-    latUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon +'&appid=0cf0c8196ec606a9f30889804aba9ea1';
+    latUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon +'&appid=0cf0c8196ec606a9f30889804aba9ea1' + '&units=imperial';
     
     fetch(latUrl)
     .then(function(response){
         return response.json();
     })
     .then(function (data){
-        console.log(data);
-        console.log(data.city.name);
-        console.log(data.list[0].dt_txt);
-        console.log(data.list[0].weather[0].icon);
-        console.log(data.list[0].main.temp);
-        console.log(data.list[0].main.humidity);
-        console.log(data.list[0].wind.speed);
+        console.log(weatherResults);
+        // console.log(data.city.name);
+        // console.log(data.list[0].dt_txt);
+        // console.log(data.list[0].weather[0].icon);
+        // console.log(data.list[0].main.temp);
+        // console.log(data.list[0].main.humidity);
+        // console.log(data.list[0].wind.speed);
+        var weatherResults = [
+            data.city.name, 
+            data.list[0].dt_txt,
+            data.list[0].weather[0].icon,
+            data.list[0].main.temp,
+            data.list[0].main.humidity,
+            data.list[0].wind.speed,
+        ];
+        displayData(weatherResults);
     })
     .catch(function (error){
         console.log(error);
     })
     
 }; 
-// city name : city.name 
-//     the date: list[0].dt_txt
-//     icon representation of weather conditions: list[0].weather[0].icon
-//     temperature: list[0].main.temp || get back in Kelvin
-//     humidity: list[0].main.humidity
-//     wind speed: list[0].wind.speed
-// 
-
-// function temperatureConverter(valNum) {
-//     valNum = parseFloat(valNum);
-//     document.getElementById("outputCelsius").innerHTML = ((valNum-273.15)*1.8)+32;
-//   }
+//city name : city.name 
+//the date: list[0].dt_txt
+//icon representation of weather conditions: list[0].weather[0].icon
+//temperature: list[0].main.temp || get back in Kelvin
+//humidity: list[0].main.humidity
+//wind speed: list[0].wind.speed
 
 
 
 var handleSearch = function (event){
     event.preventDefault();
-    console.log('button clicked');
     
     var city = cityInput.value.trim(); 
-    console.log(city);
+
     getCoordinates(city);
 };
 
